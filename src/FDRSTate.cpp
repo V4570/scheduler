2cce535bd25ca6af38508b77171142747f440060
v4570 <vasilis.tos@gmail.com>
2021-05-16 21:05:28 +0300

    Initial commit.

---
+++ src/FDRSTate.cpp
@@ -0,0 +1,116 @@
+#include "FDRSTate.h"
+#include <string>
+using namespace std;
+
+FDRSTate::FDRSTate( vector <Action> &a, vector <Rigid> &r,vector <Fact>s):rigids(r),actions(a)
+{
+//    rigids=r;
+  //  actions=a;
+    vars = s;
+}
+
+vector<GroundedAction> FDRSTate::findForks()
+{
+    vector<vector<Parameter>> solutions;
+    vector<GroundedAction> forks;
+
+    for (int i =0;i<actions.size();i++)
+    {
+        solutions = actions[i].isApplicable(rigids,vars);
+        for (int k=0;k<solutions.size();k++)
+            forks.push_back(GroundedAction(&actions[i],solutions[k]));
+    }
+    return forks;
+}
+
+vector<FDRSTate *> FDRSTate::expand()
+{
+    vector<FDRSTate *> children;
+    vector<GroundedAction> gacts;
+
+    gacts = findForks();
+
+    for (int i=0;i<gacts.size();i++)
+        children.push_back(getSuccessorState(gacts[i]));
+    return children;
+}
+
+FDRSTate *FDRSTate::getSuccessorState(GroundedAction a)
+{
+    FDRSTate *r = new FDRSTate(*this);
+
+    r->addAction(a.toString());
+
+  //  cout<<r.toString()<<endl;
+
+    for (int i=0;i<a.getEffectsCount();i++)
+    {
+        Fact f = a.getEffect(i);
+
+
+
+      //  cout<<"Searcing for "<<f.toString()<<endl;
+        for (auto it = r->vars.begin();it< r->vars.end();it++)
+        {
+         //   cout<<(*it).toString();
+            if ( *(*it).getVariable() == *f.getVariable())
+            {
+              //  cout<<" YES"<<endl;
+                (*it).setValue(f.getValue());
+            }
+//            else
+  //              cout<<" NO"<<endl;
+
+        }
+    }
+    return r;
+}
+string FDRSTate::toString()const
+{
+    string s="";
+    if (vars.size()>0)
+    {
+        s="[";
+        for (int i=0;i<vars.size()-1;i++)
+            s+=vars[i].toString()+",";
+        s+=vars[vars.size()-1].toString()+"]";
+    }
+    return s;
+}
+
+bool operator == (const FDRSTate a, const FDRSTate b)
+{
+    for (int i=0;i<b.vars.size();i++)
+    {
+        bool found =false;
+        for (int j = 0; !found && j<a.vars.size();j++)
+        {
+            if (b.vars[i]==a.vars[j])
+                found =true;
+        }
+        if (!found)
+        {
+           // cout<<"not found"<<endl;
+            return false;
+        }
+    }
+    return true;
+
+//    return a==b;
+}
+unsigned long long FDRSTate::getKey() const
+{
+    unsigned long long k=0;
+
+    for (int i=0;i<vars.size();i++)
+        k+= vars[i].getNumber() * (vars[i].getDomainSize(),i);
+
+    //cout<<k<<endl;
+    return k;
+}
+/*
+bool operator >= (pair<const FDRSTate s, bool b> s43,const FDRSTate s2)
+{
+
+}
+*/
