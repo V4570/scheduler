6a0b338a229d6f4b7613d6e1d36074ad8e951699
v4570 <vasilis.tos@gmail.com>
2021-06-20 01:57:05 +0300

    Made an oopsie. Confused U with the table storing the hmax values of each fact.

--- src/FDRSTate.cpp
+++ src/FDRSTate.cpp
@@ -16,15 +16,17 @@ int FDRSTate::heuristic(State *goal) {
      */
     FDRSTate *localGoal = (FDRSTate *) goal;
 
-    vector <pair<Fact, int>> U;
+    vector <pair<Fact, int>> hmaxTable;
     for(auto i=this->problemFacts.begin(); i<this->problemFacts.end(); i++){
         if(find(this->vars.begin(), this->vars.end(), *i) != this->vars.end()){
-            U.emplace_back(*i, 0);
+            hmaxTable.emplace_back(*i, 0);
         }
         else{
-            U.emplace_back(*i, INT_MAX);
+            hmaxTable.emplace_back(*i, INT_MAX);
         }
     }
+
+    vector <Fact> U = this->vars;
 
     return 0;
 }
